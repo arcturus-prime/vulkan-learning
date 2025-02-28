6f33540f9bf772b5c6478ee64420585a5962f6de
arcturus-prime <seanans135@gmail.com>
2025-02-28 12:14:56 -0600

    Delete src/fluid.hpp

--- src/fluid.hpp
+++
@@ -1,9 +0,0 @@
-struct FluidState {
-    constexpr static size_t array_size = 50 * 50;
-
-    unsigned char flow_x[array_size];
-    unsigned char flow_y[array_size];
-    unsigned char mass[array_size];
-
-    void step();
-};
